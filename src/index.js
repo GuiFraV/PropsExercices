9b60eee975fc831dca2786ea345bf414c5bc4eb4
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-07-25 18:48:49 +0200

    Done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 
-function index() {
-  return <h1>Hello</h1>;
-}
+const userInfo = {
+  avatar: "https://example.com/avatar.jpg",
+  name: "John Doe",
+  job: "Développeur web",
+  location: "Paris, France",
+};
 
-export default index;
+const UserProfile = () => {
+  return (
+    <div>
+      <User {...userInfo} />
+    </div>
+  );
+};
+
+const User = ({ avatar, name, job, location }) => {
+  return (
+    <>
+      <img src={avatar} alt="User avatar" />
+      <h2>{name}</h2>
+      <p>{job}</p>
+      <p>{location}</p>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<UserProfile />);
+
+export default UserProfile;
